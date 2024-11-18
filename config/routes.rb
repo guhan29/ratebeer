5bb79f4fe52263b87db75d6f4a070ddb6a427b36
Guhan M <60909829+guhan29@users.noreply.github.com>
2024-11-18 14:43:28 +0000

    completed exercise 4 of week3

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
 
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
+  # get 'signout', to: 'sessions#destroy'
 
   resources :sessions, only: [:new, :create, :destroy]
 end
