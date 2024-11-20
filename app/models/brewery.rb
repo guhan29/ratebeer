2843cc0b78897b7e2484bc62d3fba7b9aca472a2
Guhan M <60909829+guhan29@users.noreply.github.com>
2024-11-20 04:08:18 +0000

    completes exercise 8 of week3

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,7 +4,7 @@ class Brewery < ApplicationRecord
   validates :name, presence: true
   validates :year, numericality: { only_integer: true,
                                    greater_than_or_equal_to: 1040,
-                                   less_than_or_equal_to: 2023 }
+                                   less_than_or_equal_to: proc { Time.now.year } }
 
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
