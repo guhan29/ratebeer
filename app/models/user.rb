7b5e28cc14ea8e9ac5fb603af6dbeace65e97e13
Guhan M <60909829+guhan29@users.noreply.github.com>
2024-11-20 07:45:14 +0000

    completed exercise 12 of week3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
   include RatingAverage
 
+  has_secure_password
+
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
+  validates :password, 
+                     format: { 
+                       with: /\A(?=.*[A-Z])(?=.*\d).{4,}\z/,
+                       message: "Password must be atleast 4 chars and must contain at least one uppercase letter and one number." 
+                     }
 
   has_many :ratings
   has_many :beers, through: :ratings
