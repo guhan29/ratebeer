603694050033607e7c70af27ba27f6a2dd2df4aa
Guhan M <60909829+guhan29@users.noreply.github.com>
2024-11-20 09:12:42 +0000

    completed exercise 15 of week3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
 
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
-  validates :password, 
-                     format: { 
-                       with: /\A(?=.*[A-Z])(?=.*\d).{4,}\z/,
-                       message: "Password must be atleast 4 chars and must contain at least one uppercase letter and one number." 
-                     }
+  validates :password,
+            format: {
+              with: /\A(?=.*[A-Z])(?=.*\d).{4,}\z/,
+              message: "Password must be atleast 4 chars and must contain at least one uppercase letter and one number."
+            }
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
 end
